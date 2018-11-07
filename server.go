53abd6af2ec159b1ac1c196154d4f6bacfc61649
bruce <mcmoranbjr@gmail.com>
2018-11-06 17:23:44 -0800

    fix: not enough features
    Look at the captured features instead of maxFeatures

--- server.go
+++ server.go
@@ -5,14 +5,21 @@ import "net"
 import "encoding/binary"
 import "io"
 import "gocv.io/x/gocv"
-
+import "os"
 import "image"
 import "image/color"
 
+var (
+	blue = color.RGBA{0, 0, 255, 0}
+	red  = color.RGBA{255, 100, 100, 90}
+)
+
 func listen(imageChan chan []byte) {
+	fmt.Println("Starting listener")
 	l, err := net.Listen("tcp", ":8000")
 	if err != nil {
-		panic(err)
+		fmt.Println("failed to listen", err)
+		os.Exit(1)
 	}
 	defer l.Close()
 	fmt.Println("Listening...")
@@ -53,24 +60,47 @@ func LittleEndianStructHandler(c net.Conn, imageChan chan []byte) {
 func jpegToMat(img []byte) (gocv.Mat, error) {
 	return gocv.IMDecode(img, gocv.IMReadColor)
 }
-func featureExtractor(maxFeatures int, mat gocv.Mat) gocv.Mat {
+func featureExtractor(maxFeatures int, mat gocv.Mat) []image.Point {
 	// Required for tracking features
 	grayImage := gocv.NewMat()
 	gocv.CvtColor(mat, &grayImage, gocv.ColorBGRToGray)
-	red := color.RGBA{255, 100, 100, 90}
 	corners := gocv.NewMat()
 	gocv.GoodFeaturesToTrack(grayImage, &corners, maxFeatures, 0.01, 1.0)
 	// Corners is a 2 dim array [ [x,y]...]
-	for f := 0; f < maxFeatures; f++ {
+	points := []image.Point{}
+	for f := 0; f < corners.Rows(); f++ {
 		x := corners.GetFloatAt(f, 0)
 		y := corners.GetFloatAt(f, 1)
 		// Using GetInt yields out of range results
-		point := image.Pt(int(x), int(y))
+		points = append(points, image.Pt(int(x), int(y)))
+	}
+	return points
+}
+
+// Trackable descriptors are rectangles
+func detectTrackableFeatures(features []image.Point) []image.Rectangle {
+	rects := []image.Rectangle{}
+	for _, feature := range features {
+		// Create a 3x3 box
+		r := image.Rect(
+			feature.X-1,
+			feature.Y-1,
+			feature.X+1,
+			feature.Y+1)
+		rects = append(rects, r)
+	}
+
+	return rects
+}
+
+func circleFeatures(features []image.Point, mat gocv.Mat) gocv.Mat {
+	for _, feature := range features {
 		go func(mat *gocv.Mat, pt image.Point) {
-			gocv.Circle(mat, point, 4, red, 2)
-		}(&mat, point)
+			gocv.Circle(mat, pt, 4, red, 2)
+		}(&mat, feature)
 	}
 	return mat
+
 }
 
 // Demo: Accepts images over the wire in [4 byte len of image, imagebytes] format
@@ -86,7 +116,9 @@ func main() {
 			fmt.Println("unable to convert mat", err)
 			break
 		}
-		mat = featureExtractor(500, mat)
+		features := featureExtractor(500, mat)
+		mat = circleFeatures(features, mat)
+		// Compute descriptors
 		window.IMShow(mat)
 		if key := window.WaitKey(1); key == 113 { // 'q'
 			break
